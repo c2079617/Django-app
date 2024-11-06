1eb811bb7d54b8a99fb6092f1a1e802edbf3e1f0
c2079617 <c2079617@hallam.shu.ac.uk>
2024-11-06 16:17:19 +0000

    Created Profile signals and default picture

--- users/models.py
+++ users/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png', upload_to='profile_pics')
-
+    image = models.ImageField(default='media/profile_pics/default.png',upload_to='profile_pics')
 def __str__(self):
     return f'{self.user.first_name} {self.user.last_name}'
